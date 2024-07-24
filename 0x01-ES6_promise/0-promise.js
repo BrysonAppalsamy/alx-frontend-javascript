4041b2683b15273a9aacca4054f12c18ceb90f22
brysonappalsamy <brysonappalsamy@gmail.com>
2024-07-24 22:44:51 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
-export default function getResponseFromAPI(){
-  return new promise(() => {});
+export default function getResponseFromAPI() {
+  const success = true;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
